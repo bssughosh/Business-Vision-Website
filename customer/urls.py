fe778ad4ddc0aa08b28350cde1fca09e98e0395e
bssughosh <bssughosh27@gmail.com>
2020-01-17 22:53:03 +0530

    product display working

--- customer/urls.py
+++ customer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.c1, name='cus'),
+    path('display/', views.dis, name='display'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from seller.models import ProductData
+
 
 # Create your views here.
 def c1(request):
     return render(request, 'customer/c1.html')
+
+
+def dis(request):
+    x = ProductData.objects.all()
+    return render(request, 'customer/product_display.html', {'prods': x})

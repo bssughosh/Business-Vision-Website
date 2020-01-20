80f216e7c4e00c0f85c1c79fe6248a04b3fd84df
bssughosh <bssughosh27@gmail.com>
2020-01-20 19:47:50 +0530

    something

--- customer/views.py
+++ customer/views.py
@@ -11,25 +11,36 @@ def c1(request):
 
 def dis(request):
     x = ProductData.objects.all()
+    # if request.method == 'POST':
+    #     pid = request.POST.get('pid', False)
+    #     print(pid)
+    flag = False
+    if request.method == 'POST':
+        s = request.POST.get('pname',False)
+        if not s:
+            for y in x:
+                if s == y.p_name:
+                    print('Found')
+                    flag =True
+                    return render(request, 'customer/product_display.html', {'prods1': y})
+
     return render(request, 'customer/product_display.html', {'prods': x})
 
 
-@csrf_exempt
 def desc(request):
-    if request.method == 'POST':
-        pid = request.body
-        pid1 = pid.split('-')
-        pid = pid1[1]
-        print(pid)
-        x = ProductData.objects.all()
-        y = x[int(pid)]
+    # if request.method == 'POST':
+    #     pid = request.body
+    #     pid1 = pid.split('-')
+    #     pid = pid1[1]
+    #     print(pid)
+    #     x = ProductData.objects.all()
+    #     y = x[int(pid)]
+    #
+    # else:
+    #     y = ProductData()
+    return render(request, 'customer/product_description.html')
 
-    else:
-        y = ProductData()
-    return render(request, 'customer/product_description.html', {'data': y})
 
-
-@csrf_exempt
 def profile(request):
     x = UserData.objects.all()
     for y in x:

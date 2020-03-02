f8f6c4d44af8d98b342edc12e6c832e36a130c80
bssughosh <bssughosh27@gmail.com>
2020-03-02 18:50:30 +0530

    cart done

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.showcart, name='showcart')
+    path('', views.showcart, name='showcart'),
+    path('<int:object_id>/<int:object_id1>/', views.removecart, name='remove'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from seller.models import ProductData
-from carts.models import PCart
+from carts.models import PCart, Quantity
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -10,5 +11,23 @@ def showcart(request):
         p = PCart(user=request.user.email)
         p.save()
     p1 = PCart.objects.get(user__iexact=request.user.email)
-    print(p1.quant.values())
-    return render(request, 'carts/showcart.html', {'prod': p1.p_name.values(), 'quant': p1.quant.values()})
+    total = 0
+    x = list(p1.p_name.values())
+    y = list(p1.quant.values())
+    for i, j in zip(x, y):
+        total += int(i['p_price']) * int(j['min_q'])
+    return render(request, 'carts/showcart.html', {'prod': p1, 'total': total})
+
+
+def removecart(request, object_id, object_id1):
+    num_res = PCart.objects.filter(user=request.user.email).count()
+    if num_res == 0:
+        p = PCart(user=request.user.email)
+        p.save()
+    p1 = PCart.objects.get(user__iexact=request.user.email)
+    q1 = get_object_or_404(ProductData, id=object_id)
+    q2 = get_object_or_404(Quantity, id=object_id1)
+    p1.p_name.remove(q1)
+    p1.quant.remove(q2)
+    p1.save()
+    return redirect('/cart')

bdb501261352f0797483dff3ff15b86ce89f3ad9
bssughosh <bssughosh27@gmail.com>
2020-03-10 20:20:06 +0530

    added customer description

--- seller/urls.py
+++ seller/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('my_prod/', views.my_prod, name='my_prod'),
     path('browse/', views.browse, name='browse'),
     path('edit/<int:object_id>/', views.editpage, name='editpage'),
+    re_path(r'^description/(?P<object_id>\d+)/$', views.desc, name='description'),
+    re_path(r'^description1/(?P<object_id>\d+)/$', views.desc1, name='description1'),
     # path('deleter/<int:object_id>/', views.delete_record, name='delete_record'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProductUploadForm
 from django.contrib.auth.models import User, auth
 from accounts.models import SellerData
-from .models import ProductData
+from .models import ProductData, Rating, UsersRated
 from django.db.models import Q
 from django.contrib import messages
 import os
@@ -94,3 +94,59 @@ def delete_record(request, object_id):
     os.remove(file)
     messages.info(request, 'Product Deleted')
     return redirect('/seller')
+
+
+def desc(request, object_id):
+    product = ProductData.objects.get(id=object_id)
+    sellers = SellerData.objects.all()
+    list_products = ProductData.objects.filter(p_name__icontains=product.p_name).order_by('-p_price')
+    p1 = ProductData.objects.filter(id=list_products[0].id).values()
+    s = p1[0]['seller_name']
+    num_res1 = Rating.objects.filter(seller=s).count()
+    if num_res1 == 0:
+        co = 0
+        us = []
+    else:
+        co = Rating.objects.filter(seller=s).values()[0]['rate']
+        us1 = Rating.objects.get(seller=s)
+        us = us1.users.values_list()
+    if request.method == 'POST' and 'deleter' in request.POST:
+        Q = request.POST.get('quant', list_products[0].min_q)
+        num_res = PCart.objects.filter(user=request.user.email).count()
+        if num_res == 0:
+            p = PCart(user=request.user.email)
+            p.save()
+        q = Quantity(min_q=Q)
+        q.save()
+        p1 = PCart.objects.get(user__iexact=request.user.email)
+        p1.p_name.add(list_products[0])
+        p1.quant.add(q)
+        p1.save()
+    return render(request, 'seller/product_description.html',
+                  {'data': product, 'listdata': list_products, 't': 0, 'co': co, 'us': us, 'sellers': sellers})
+
+
+def desc1(request, object_id):
+    product = ProductData.objects.get(id=object_id)
+    list_products = ProductData.objects.filter(p_name__icontains=product.p_name).order_by('-p_price')
+    p1 = ProductData.objects.filter(id=product.id).values()
+    s = p1[0]['seller_name']
+    num_res1 = Rating.objects.filter(seller=s).count()
+    if num_res1 == 0:
+        co = 0
+    else:
+        co = Rating.objects.filter(seller=s).values()[0]['rate']
+    if request.method == 'POST' and 'deleter' in request.POST:
+        Q = request.POST.get('quant', product.min_q)
+        num_res = PCart.objects.filter(user=request.user.email).count()
+        if num_res == 0:
+            p = PCart(user=request.user.email)
+            p.save()
+        q = Quantity(min_q=Q)
+        q.save()
+        p1 = PCart.objects.get(user__iexact=request.user.email)
+        p1.p_name.add(product)
+        p1.quant.add(q)
+        p1.save()
+    return render(request, 'seller/product_description.html',
+                  {'data': product, 'listdata': list_products, 't': 1, 'co': co})
